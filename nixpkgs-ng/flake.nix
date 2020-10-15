1841354c88c7c02b0cd314b5003ccff5255d5d16
Eelco Dolstra <edolstra@gmail.com>
2020-10-15 22:58:51 +0200

    Formatting

--- nixpkgs-ng/flake.nix
+++ nixpkgs-ng/flake.nix
@@ -1,122 +1,118 @@
 {
   outputs = { self, nixpkgs }: let pkgs = import nixpkgs{ system = "x86_64-linux"; }; in {
 
-    modules = {
+    modules.derivation = module {
+      doc = "A Nix derivation.";
 
-      derivation = module {
-        doc = "A Nix derivation.";
+      options = {
 
-        options = {
-
-          name = {
-            example = "hello-1.2.3";
-            doc = "The name component of the store paths produced by this derivation.";
-          };
-
-          system = {
-            default = "x86_64-linux";
-            example = "x86_64-darwin";
-            doc = "The platform type on which to build this derivation.";
-          };
-
-          derivation = {
-            doc = "The resulting derivation.";
-          };
-
-          environment = {
-            default = {};
-            doc = "Environment variables passed to the builder.";
-          };
-
-          # TODO: outputs, builder, args, ...
-
-          buildCommand = {
-            doc = "The contents of the shell script that builds the derivation.";
-          };
-
+        name = {
+          example = "hello-1.2.3";
+          doc = "The name component of the store paths produced by this derivation.";
         };
 
-        config = { config }: {
+        system = {
+          default = "x86_64-linux";
+          example = "x86_64-darwin";
+          doc = "The platform type on which to build this derivation.";
+        };
 
-          derivation = derivation ({
-            __structuredAttrs = true;
-            name = config.name;
-            system = config.system;
-            builder = "${pkgs.bash}/bin/bash";
-            args = [ "-c" ("source .attrs.sh; out=\${outputs[out]}; " + config.buildCommand) ];
-          } // config.environment);
+        derivation = {
+          doc = "The resulting derivation.";
+        };
 
+        environment = {
+          default = {};
+          doc = "Environment variables passed to the builder.";
+        };
+
+        # TODO: outputs, builder, args, ...
+
+        buildCommand = {
+          doc = "The contents of the shell script that builds the derivation.";
         };
 
       };
 
-      stdenv = module {
-        doc =
-          ''
-            This module provides a standard environment for building
-            packages.
-          '';
+      config = { config }: {
 
-        extends = [ self.modules.derivation ];
-
-        config = { config }: {
-          environment.PATH = "${pkgs.coreutils}/bin:${pkgs.gnutar}/bin";
-        };
-      };
-
-      package = module {
-        doc = "An installable package.";
-
-        extends = [ self.modules.derivation ];
-
-        options = {
-
-          pname = {
-            example = "hello";
-            doc = "Name of the package.";
-          };
-
-          version = {
-            default = null;
-            example = "1.2.3";
-            doc = "The version of the package. Must be null or start with a digit.";
-          };
-
-        };
-
-        config = { config }: {
-          name = "${config.pname}${if config.version != null then "-" + config.version else ""}";
-        };
+        derivation = derivation ({
+          __structuredAttrs = true;
+          name = config.name;
+          system = config.system;
+          builder = "${pkgs.bash}/bin/bash";
+          args = [ "-c" ("source .attrs.sh; out=\${outputs[out]}; " + config.buildCommand) ];
+        } // config.environment);
 
       };
 
-      bundle = module {
+    };
 
-        extends = [ self.modules.derivation ];
+    modules.stdenv = module {
+      doc =
+        ''
+          This module provides a standard environment for building
+          packages.
+        '';
 
-        options = {
+      extends = [ self.modules.derivation ];
 
-          bundle = {
-            doc = "A derivation that produces a tarball containing the closure of a package.";
+      config = { config }: {
+        environment.PATH = "${pkgs.coreutils}/bin:${pkgs.gnutar}/bin";
+      };
+    };
+
+    modules.package = module {
+      doc = "An installable package.";
+
+      extends = [ self.modules.derivation ];
+
+      options = {
+
+        pname = {
+          example = "hello";
+          doc = "Name of the package.";
+        };
+
+        version = {
+          default = null;
+          example = "1.2.3";
+          doc = "The version of the package. Must be null or start with a digit.";
+        };
+
+      };
+
+      config = { config }: {
+        name = "${config.pname}${if config.version != null then "-" + config.version else ""}";
+      };
+
+    };
+
+    modules.bundle = module {
+
+      extends = [ self.modules.derivation ];
+
+      options = {
+
+        bundle = {
+          doc = "A derivation that produces a tarball containing the closure of a package.";
+        };
+
+      };
+
+      config = { config } @ outer: {
+
+        bundle = (module {
+          extends = [ self.modules.stdenv ];
+          config = { config }: {
+            name = "${outer.config.pname}-closure-${outer.config.version}";
+            buildCommand =
+              ''
+                mkdir $out
+                tar cvf $out/bundle.tar ${outer.config.derivation}
+              '';
           };
-
-        };
-
-        config = { config } @ outer: {
-
-          bundle = (module {
-            extends = [ self.modules.stdenv ];
-            config = { config }: {
-              name = "${outer.config.pname}-closure-${outer.config.version}";
-              buildCommand =
-                ''
-                  mkdir $out
-                  tar cvf $out/bundle.tar ${outer.config.derivation}
-                '';
-            };
-          }).final.derivation;
-
-        };
+        }).final.derivation;
 
       };
 
